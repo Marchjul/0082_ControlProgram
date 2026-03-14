bea6cee19f4ed2413ea909dd8f62e91eb0661831
Marchjul <marchellogiovani28@gmail.com>
2026-03-14 12:28:58 +0700

    Hitung rerata menggunakan fungsi Rata_rata()

--- nestedif.cpp
+++ nestedif.cpp
@@ -26,6 +26,15 @@ int main(){
     cout << "Masukkan nilai Matematika = ";
     cin >> nilMT;
 
+    //rerata = (nilBI+nilMT)/2;
+    //if (rerata >= 60)
+    //    status = "Lolos";
+    //else
+    //    status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
+
+    
 
 
 
